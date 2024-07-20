fa4699e0ab4eb327e439053da04604371ed994cc
Vivek Kumar <vivek3553.vr@gmail.com>
2024-07-20 13:49:55 +0530

    change copy functionality

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export default function App(){
 
   const handleCopyToClipboard = () => {
     copy(generatedPassword);
-    // alert('Password copied to clipboard!');
+    alert('Password copied to clipboard!');
   };
 
   return (
